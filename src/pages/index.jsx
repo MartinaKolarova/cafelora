9b061eaa14a9b78dc4dddf306efe6c328effee83
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-04-27 10:54:19 +0200

    Final components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,19 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { Menu } from '../components/Menu';
 
-document.querySelector('#root').innerHTML = render();
+document.querySelector('#root').innerHTML = render(
+  <div className="page">
+    <Header />
+    <main>
+      <Banner />
+      <Menu />
+      <Gallery />
+      <Contact />
+    </main>
+    <Footer />
+  </div>,
+);
