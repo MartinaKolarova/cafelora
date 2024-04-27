d7d89eedf1f16601f4790f73bf6a3379a6fcd983
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-04-27 16:58:23 +0200

    Added props showMenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,40 @@
 import './Header.css';
 
-export const Header = () => {
-  return (
-    <header classID="header">
-      <div className="header__content container">
-        <div className="site-logo"></div>
+/*Na stránce použijte komponenty Header a Footer. Komponentu Header bude potřeba upravit, protože na stránce s objednávkou se hlavička zobrazuje bez hlavního menu. Do komponenty Header tedy přidejte prop s názvem showMenu s výchozí hodnotou true. Pokud bude showMenu mít hodnotu false, komponenta Header zobrazí hlavičku pouze s odkazem na hlavní stránku, jak je navrženo v zadání projektu v souboru order.jsx.*/
 
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
+export const Header = ({ showMenu = true }) => {
+  if (showMenu) {
+    return (
+      <header classID="header">
+        <div className="header__content container">
+          <div className="site-logo"></div>
+
+          <div className="navigation">
+            <button className="nav-btn"></button>
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+              <a href="#menu">menu</a>
+              <a href="#gallery">galerie</a>
+              <a href="#contact">kontakt</a>
+            </nav>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header classID="header">
+        <div className="header__content container">
+          <div className="site-logo"></div>
+
+          <div className="navigation">
+            <button className="nav-btn"></button>
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  }
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,18 @@
-import { render } from "@czechitas/render";
-import "../global.css";
-import "./index.css";
-import { Header } from "../components/Header";
-import { Banner } from "../components/Banner";
-import { Menu } from "../components/Menu";
-import { Gallery } from "../components/Gallery";
-import { Contact } from "../components/Contact";
-import { Footer } from "../components/Footer";
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { Menu } from '../components/Menu';
+import { Gallery } from '../components/Gallery';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
 
-const response = await fetch("http://localhost:4000/api/drinks");
+const response = await fetch('http://localhost:4000/api/drinks');
 const drinksData = await response.json();
 const drinksList = drinksData.data;
 
-document.querySelector("#root").innerHTML = render(
+document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
     <main>
@@ -23,12 +23,12 @@ document.querySelector("#root").innerHTML = render(
       <Contact />
     </main>
     <Footer />
-  </div>
+  </div>,
 );
 
 const menuSwitch = () => {
-  const navElement = document.querySelector(".rollout-nav");
-  navElement.classList.toggle("nav-closed");
+  const navElement = document.querySelector('.rollout-nav');
+  navElement.classList.toggle('nav-closed');
 };
 
 const showId = async (event) => {
@@ -38,27 +38,27 @@ const showId = async (event) => {
   const responseId = await fetch(
     `http://localhost:4000/api/drinks/${actualId}`,
     {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify([
         {
-          op: "replace",
-          path: "/ordered",
+          op: 'replace',
+          path: '/ordered',
           value: !drinksList[actualId].ordered,
         },
       ]),
-    }
+    },
   );
   const responseBody = await responseId.json();
   window.location.reload();
 };
 
-document.querySelector(".nav-btn").addEventListener("click", menuSwitch);
+document.querySelector('.nav-btn').addEventListener('click', menuSwitch);
 
-document.querySelector(".rollout-nav").addEventListener("click", menuSwitch);
+document.querySelector('.rollout-nav').addEventListener('click', menuSwitch);
 
-document.querySelectorAll(".drink__controls").forEach((button) => {
-  button.addEventListener("submit", showId);
+document.querySelectorAll('.drink__controls').forEach((button) => {
+  button.addEventListener('submit', showId);
 });
